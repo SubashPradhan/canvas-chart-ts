25683259fe2cd4cc78ead8278f408cb387f80571
Subash Pradahan <suprowilde5@gmail.com>
2020-12-19 15:25:49 +0100

    Webpack configuration

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-const firstName: string = 'Subash';
-console.log(firstName);
+const head: HTMLHeadElement = document.head;
+const style: HTMLStyleElement = document.createElement('style');
+head.appendChild(style);
+
+style.innerHTML = `
+  *{
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+console.log('Hello World');
---
+++ src/modules/test.ts
@@ -0,0 +1,1 @@
+export const firstName: string = 'subash';
