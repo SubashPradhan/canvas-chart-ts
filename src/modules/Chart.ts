f9c1e16de3809941d79ace496d45668f3a6a064e
Subash Pradahan <suprowilde5@gmail.com>
2021-01-15 12:53:35 +0100

    Refactor code for highest and lowest starting points

--- src/modules/Chart.ts
+++ src/modules/Chart.ts
@@ -1,39 +1,50 @@
 import { givenData } from './helper/data';
 import { countBlocks } from './helper/countBlocks';
+import {
+	getLowestStartPrice,
+	getHighestEndingPrice,
+} from './helper/getPriceRange';
 
 interface IChart {
 	drawGrids: () => void;
 	drawTimeLine: () => void;
 }
+
 class Chart implements IChart {
 	public container: HTMLDivElement;
 	private xGrid: number = 10;
 	private yGrid: number = 10;
-	private margin: number = 100;
 	private tabcellSize: number = 10;
+	public currentStartingLowPrice: number = getLowestStartPrice(givenData);
+	public currentEndingHighPrice: number = getHighestEndingPrice(givenData);
+	private screenMargin: number = 100;
 	private canvas: HTMLCanvasElement = document.createElement('canvas');
 	public canvasHeight: number = (this.canvas.height =
-		document.documentElement.clientHeight - this.margin);
+		document.documentElement.clientHeight - this.screenMargin);
 	public canvasWidth: number = (this.canvas.width =
-		document.documentElement.clientWidth - this.margin);
+		document.documentElement.clientWidth - this.screenMargin);
 	public ctx: CanvasRenderingContext2D = this.canvas.getContext('2d');
 
 	public constructor(container: HTMLDivElement) {
 		this.container = container;
 		this.container.appendChild(this.canvas);
-		this.ctx.beginPath();
 	}
 
 	public drawGrids() {
+		this.ctx.beginPath();
+		// Draw Y grids
 		while (this.yGrid < this.canvasWidth) {
-			this.ctx.moveTo(this.canvasWidth - this.yGrid, 0);
-			this.ctx.lineTo(this.canvasWidth - this.yGrid, this.canvasHeight);
+			let xPoint: number = this.canvasWidth - this.yGrid;
+			this.ctx.moveTo(xPoint, 0);
+			this.ctx.lineTo(xPoint, this.canvasHeight);
 			this.yGrid += this.tabcellSize;
 		}
 
+		// Draw x grids
 		while (this.xGrid < this.canvasHeight) {
-			this.ctx.moveTo(0, this.canvasHeight - this.xGrid);
-			this.ctx.lineTo(this.canvasWidth, this.canvasHeight - this.xGrid);
+			let yPoint: number = this.canvasHeight - this.xGrid;
+			this.ctx.moveTo(0, yPoint);
+			this.ctx.lineTo(this.canvasWidth, yPoint);
 			this.xGrid += this.tabcellSize;
 		}
 		this.ctx.strokeStyle = 'grey';
@@ -42,76 +53,26 @@ class Chart implements IChart {
 
 	public drawTimeLine() {
 		let yBaseLine: number = this.canvasHeight - countBlocks(5);
-		// let startingPoint: number = this.canvasWidth - countBlocks(5);
-		// let differenceBetweenPoints: number = 50;
 		this.ctx.beginPath();
 		this.ctx.moveTo(0, yBaseLine);
 		this.ctx.lineTo(this.canvasWidth, yBaseLine);
-		// this.ctx.stroke();
-		// const data: (string | number)[][] = givenData;
-		// const time = givenData.map(prices => {
-		// Change given timestamps to UTC time format
-		// let currentTime: string = new Date((prices[0] as number) / 1000)
-		// .toLocaleString()
-		// 		// .replace(/^(\w+|\s)|(\w+)$/g, '');
-		// 	let openingPrice: string | number = prices[1];
-		// 	let highestPrice: string | number = prices[2];
-		// 	let lowestPrice: string | number = prices[3];
-		// 	let closingPrice: string | number = prices[4];
-		// 	this.ctx.moveTo(startingPoint, yBaseLine);
-		// 	this.ctx.lineTo(startingPoint, yBaseLine + 10);
-		// 	this.ctx.fillText(`${currentTime}`, startingPoint, yBaseLine + 15);
-		// 	this.ctx.textAlign = 'center';
-		// 	startingPoint -= differenceBetweenPoints;
-		// });
-		// this.ctx.strokeStyle = 'red';
-		// this.ctx.stroke();
 	}
-	// REFACTOR THIS CODE LATER
-	public listOfLowestPrice: (string | number)[] = givenData.map(
-		prices => prices[3],
-	);
-
-	// Change this name to lowestPrice
-	public currentLowestPoint: number = Math.trunc(
-		Math.min(...(this.listOfLowestPrice as number[])),
-	);
-
-	//Current min/ lowest point
-	public currentStartingLowPoint: number =
-		this.currentLowestPoint % 5 === 0
-			? this.currentLowestPoint
-			: this.currentLowestPoint - (this.currentLowestPoint % 5); //This needs to be checked as well
-
-	// For creating range between highest and lowest price.
-	public listOfHighestPrice: (string | number)[] = givenData.map(
-		prices => prices[2],
-	);
-
-	public currentHighestPrice = Math.trunc(
-		Math.max(...(this.listOfHighestPrice as number[])),
-	);
-
-	public currentEndingHighestPrice =
-		this.currentHighestPrice % 5 === 0
-			? this.currentHighestPrice
-			: this.currentHighestPrice +
-			  countBlocks(5) + // In this scenario I want atleast a top to have countblocks of 5 gap
-			  (5 - (this.currentHighestPrice % 5));
 
 	public priceIncrementOnBlocks: number = Math.floor(
 		countBlocks(
-			(this.currentEndingHighestPrice - this.currentStartingLowPoint) /
+			(this.currentEndingHighPrice - this.currentStartingLowPrice) /
 				(this.canvasHeight - countBlocks(5)),
 		),
 	); // prices per block times number of blocks.
+
 	public drawPriceLine() {
+		console.log(this.currentStartingLowPrice);
 		const xBaseLine = this.ctx.moveTo(this.canvasWidth - countBlocks(5), 0);
 		this.ctx.lineTo(this.canvasWidth - countBlocks(5), this.canvasHeight);
 		let startingPointForY = this.canvasHeight - countBlocks(5);
 		let startingPointForX = this.canvasWidth - countBlocks(5);
 
-		let Yline: number = this.currentStartingLowPoint; // Change this as well
+		let Yline: number = this.currentStartingLowPrice; // Change this as well
 		let priceIncrement: number = this.priceIncrementOnBlocks * 5; // per block holds the price of price increment so per 5 blocks which is our increment for line Y
 		while (startingPointForY >= 0) {
 			this.ctx.moveTo(startingPointForX, startingPointForY);
